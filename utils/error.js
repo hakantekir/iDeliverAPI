3a48a05df7645acae05d93137724b568c8f04c2c
Hakan <hakantekir2002@outlook.com>
2023-04-04 05:14:48 +0300

    fix: Add missing errors into module

--- utils/error.js
+++ utils/error.js
@@ -6,6 +6,8 @@ const missingCredentialsError = createError(101, 'Missing credentials', 'Please 
 const invalidCredentialsError = createError(102, 'Invalid credentials', 'The email or password you entered is incorrect');
 const emailExistsError = createError(103, 'Email already exists', 'This email address is already in use');
 const passwordTooShortError = createError(104, 'Password too short', 'Password must be at least 8 characters');
+const invalidTokenError = createError(201, 'Invalid token', 'The token you provided is invalid');
+const expiredTokenError = createError(202, 'Expired token', 'The token you provided has expired');
 const serverError = createError(500, 'Server error', 'Something went wrong');
 
 module.exports = {
@@ -13,5 +15,7 @@ module.exports = {
     invalidCredentialsError,
     emailExistsError,
     passwordTooShortError,
+    invalidTokenError,
+    expiredTokenError,
     serverError
 }
